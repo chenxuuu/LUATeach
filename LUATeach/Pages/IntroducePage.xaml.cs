1459e14a88b1dc6f0a817d956bd9327324bd1bf8
chenxuuu <lolicon@papapoi.com>
2019-08-14 12:50:25 +0800

    选择题选项随机化

--- LUATeach/Pages/IntroducePage.xaml.cs
+++ LUATeach/Pages/IntroducePage.xaml.cs
@@ -25,15 +25,49 @@ namespace LUATeach.Pages
             InitializeComponent();
         }
 
+        private string[] options;
+        private int truth = Global.Levels.LevelList[Global.Levels.selected].choice - 1;
+
+        Random r = new Random();
+        private void ReLoadChoice()
+        {
+            Console.WriteLine("last" + truth);
+            for (int i = 0; i < 4; i++)
+            {
+                int temp = r.Next(0, 4);
+                string t = options[i];
+                options[i] = options[temp];
+                options[temp] = t;
+                if (i == truth)
+                    truth = temp;
+                else if (temp == truth)
+                    truth = i;
+                Console.WriteLine($"swap{i},{temp}");
+                Console.WriteLine("result" + truth);
+            }
+            Console.WriteLine("=======================");
+            option1.Content = options[0];
+            option2.Content = options[1];
+            option3.Content = options[2];
+            option4.Content = options[3];
+        }
+
+
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             titleTextBlock.Text = Global.Levels.LevelList[Global.Levels.selected].title;
             Viewer.Markdown = Global.Levels.LevelList[Global.Levels.selected].question;
             questionTextBlock.Text = Global.Levels.LevelList[Global.Levels.selected].choiceTitle;
-            option1.Content = Global.Levels.LevelList[Global.Levels.selected].choices[0];
-            option2.Content = Global.Levels.LevelList[Global.Levels.selected].choices[1];
-            option3.Content = Global.Levels.LevelList[Global.Levels.selected].choices[2];
-            option4.Content = Global.Levels.LevelList[Global.Levels.selected].choices[3];
+
+            options = new string[]
+            {
+                Global.Levels.LevelList[Global.Levels.selected].choices[0],
+                Global.Levels.LevelList[Global.Levels.selected].choices[1],
+                Global.Levels.LevelList[Global.Levels.selected].choices[2],
+                Global.Levels.LevelList[Global.Levels.selected].choices[3],
+            };
+
+            ReLoadChoice();
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
@@ -43,30 +77,33 @@ namespace LUATeach.Pages
 
         private void CheckResult(int select)
         {
-            if (Global.Levels.LevelList[Global.Levels.selected].choice == select)
+            if (truth == select)
                 this.NavigationService.Navigate(new Uri("Pages/RightPage.xaml", UriKind.Relative));
             else
-                this.NavigationService.Navigate(new Uri("Pages/WrongPage.xaml", UriKind.Relative));
+            {
+                MessageBox.Show("答错啦，再仔细想想吧", "选择结果", MessageBoxButton.OK, MessageBoxImage.Error);
+                ReLoadChoice();
+            }
         }
 
         private void Option1_Click(object sender, RoutedEventArgs e)
         {
-            CheckResult(1);
+            CheckResult(0);
         }
 
         private void Option2_Click(object sender, RoutedEventArgs e)
         {
-            CheckResult(2);
+            CheckResult(1);
         }
 
         private void Option3_Click(object sender, RoutedEventArgs e)
         {
-            CheckResult(3);
+            CheckResult(2);
         }
 
         private void Option4_Click(object sender, RoutedEventArgs e)
         {
-            CheckResult(4);
+            CheckResult(3);
         }
     }
 }
